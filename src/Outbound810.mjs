304e8c0743171dcee72f4092301326fd9d81fe70
The Todd <68481229+tajenay@users.noreply.github.com>
2023-09-13 17:48:37 -0400

    Initial commit

---
+++ src/Outbound810.mjs
@@ -0,0 +1,244 @@
+import './styles.css';
+import options from './810Nordstrom.json';
+
+document.getElementById("app").innerHTML = '810 Nordstom Script';
+/*
+Everything Below this Comment is for Celigo.  There is a marker below to
+Say where to stop.  The content above is for testing purposes.
+*/
+// BEGIN CELIGO CODE
+/* 
+
+We get the first record so we can use it to modify the header data BEFORE
+going into the detail level issues.  In Celigo, we get main body, which is
+basically the entire grouped record in Celigo.
+We then get firstNode, which is the information repeated on all records that
+we require as a header.
+
+*/ 
+const mainBody = options.data[0];
+const firstNode = mainBody[0];
+//  We're going to load the addresses as the array is so many levels deep that
+//  Building it as a function is untenebale.
+//
+const addr = [];
+addr.push(firstNode.N101);
+const N1_loop = [];
+const addressInformation = [];
+const addressInformationObject = {};
+const partyIdentificationObject ={};
+for (let i=0; i < firstNode.N101.length; i++) {
+    addressInformationObject.entityIdentifierCode = firstNode.N101[i];
+    addressInformationObject.name = firstNode.N102[i];
+    addressInformationObject.identificationCodeQualifier = firstNode.N103[i];
+    addressInformationObject.identificationCode = firstNode.N104[i];
+    addressInformation.push(addressInformationObject);
+    partyIdentificationObject.partyIdentification = [addressInformationObject];
+    N1_loop.push(partyIdentificationObject);
+  };
+
+// Now, we place the functions to the top so we can begug them properly
+// inserting the returns into the JSON itself to maintain integrity of 
+// the loops and braces.
+//
+// REFERENCE INFORMATION FUNCTION
+const getReferenceInformation = node => {
+  const referenceInformation = [];
+// Make sure it is an array.  Then loop through it. If not
+// write the single records and move on.
+  if (Array.isArray(node.REF01)) {
+    node.REF01.forEach((ref01, index) => {
+      // For each value in node.REF01, we're going to create a new... OBJECT
+      const referenceInformationObject = {};
+      referenceInformationObject.referenceIdentificationQualifier = ref01;
+      if (node.REF02.length >= index) {
+        // If there's a corresponding value in node.REF02, we're going to add that data
+        // to the OBJECT
+        referenceInformationObject.referenceIdentification = node.REF02[index];
+      }
+      if (node.REF03 && node.REF03.length >= index) {
+        // If there's a corresponding value in node.REF03, we're going to add that data
+        // to the OBJECT
+        referenceInformationObject.description = node.REF03[index];
+      }
+      // Then we're going to push the OBJECT onto the referenceInformation ARRAY
+      referenceInformation.push(referenceInformationObject);
+    })
+  } else {
+    const referenceInformationObject = {};
+    referenceInformationObject.referenceIdentificationQualifier = node.REF01;
+    referenceInformationObject.referenceIdentification = node.REF02;
+    referenceInformationObject.description = node.REF03;
+    referenceInformation.push(referenceInformationObject);   
+  }
+  return referenceInformation;
+}
+//  We will add a function to do the same thing with the Date and Time Qualifiers
+//  including the allowing of single NON-array data
+//
+const getDateInformation = node => {
+  const dateInformation = [];
+// Make sure it is an array.  Then loop through it. If not
+// write the single records and move on.
+  if (Array.isArray(node.DTM01)) {
+    node.DTM01.forEach((DTM01, index) => {
+      // For each value in node.DTM01, we're going to create a new... OBJECT
+      const dateInformationObject = {};
+      dateInformationObject.dateTimeQualifier = DTM01;
+      if (node.DTM02.length >= index) {
+        // If there's a corresponding value in node.DTM02, we're going to add that data
+        // to the OBJECT
+        dateInformationObject.date = node.DTM02[index];
+      }
+      if (node.DTM03 && node.DTM03.length >= index) {
+        // If there's a corresponding value in node.DTM03, we're going to add that data
+        // to the OBJECT
+        dateInformationObject.description = node.DTM03[index];
+      }
+      // Then we're going to push the OBJECT onto the dateInformation ARRAY
+      dateInformation.push(dateInformationObject);
+    })
+  } else {
+    const dateInformationObject = {};
+    dateInformationObject.dateTimeQualifier = node.DTM01;
+    dateInformationObject.dateTime = node.DTM02;
+    dateInformationObject.description = node.DTM03;
+    dateInformation.push(dateInformationObject);   
+  }
+  return dateInformation;
+}
+//
+//
+//
+
+// We will add a function to loop through the addresses the same way we 
+// looped through the reference Identification information.
+//
+// ADDRESS INFORMATION FUNCTION
+const getAddressInformation = node => {
+  const addressInformation = [];
+  const partyIdentification =[];
+  node.N101.forEach((n101, index) => {
+    const addressInformationObject = {};
+    const partyIdentificationObject ={};
+    addressInformationObject.entityIdentifierCode = n101;
+    addressInformationObject.name = node.N102[index];
+    addressInformationObject.identificationCodeQualifier = node.N103[index];
+    addressInformationObject.identificationCode = node.N104[index];
+    addressInformation.push(addressInformationObject);
+    partyIdentificationObject.partyIdentification = [addressInformationObject];
+    N1_loop.push(partyIdentificationObject);
+  })
+  return {
+    N1_loop
+  };
+}
+// This is where the items finally come in.  For this we bring in all the records.
+// Looping through these requires returning to the full data feed
+// itself and getting the products
+//
+//  ITEM LOOP
+const getItems = node => {
+  const itemInformation =[];
+  var productItemDesc = {productItemDescription:[]};
+  node.forEach((record, index) => {
+    // create the objects to load the data into
+    const itemInformationObject = {};
+    const proddescobj ={};
+    // the item loop for the baseline Item Data Invoice
+    itemInformationObject.assignedIdentification = record.IT101;
+    itemInformationObject.quantityInvoiced = record.IT102;
+    itemInformationObject.unitOrBasisForMeasurementCode = record.IT103;
+    itemInformationObject.unitPrice = record.IT104; 
+    itemInformationObject.basisOfUnitPriceCode = record.IT105;
+    itemInformationObject.productServiceIDQualifier = record.IT106;
+    itemInformationObject.productServiceID= record.IT107;
+    itemInformation.push(itemInformationObject);
+    // the description loop for the baseline Item Description
+    proddescobj.itemDescriptionTypeCode = record.PID01;
+    proddescobj.description = record.PID05;
+    productItemDesc.productItemDescription.push(proddescobj)
+  })
+  return {
+    baselineItemDataInvoice: itemInformation, 
+    PID_loop: [productItemDesc]
+  };
+}
+// Main Response, the content is built here.  This is what will be sent
+// to Orderful for processing.  All of the previous lines are functions
+// that inform this constant.
+//
+const response = {
+  sender: {
+    isaId: firstNode.ISA06
+  },
+  receiver: {
+    isaId: firstNode.ISA08
+  },
+  type: {
+    name: firstNode.typeName
+  },
+  stream: firstNode.stream,
+  message: {
+    transactionSets: [
+      {
+        transactionSetHeader: [
+          {
+            transactionSetIdentifierCode: '810',
+            transactionSetControlNumber: '0001'
+          }
+        ],
+        beginningSegmentForInvoice: [
+          {
+            date: firstNode.BIG01,
+            invoiceNumber: firstNode.BIG02,
+            date1: firstNode.BIG03,
+            purchaseOrderNumber: firstNode.BIG04,
+            releaseNumber: firstNode.BIG05,
+            changeOrderSequenceNumber: firstNode.BIG06,
+            transactionTypeCode: firstNode.BIG07
+          }
+        ],
+        referenceInformation: getReferenceInformation(firstNode), 
+        N1_loop,
+        termsOfSaleDeferredTermsOfSale: [
+                {
+                  termsTypeCode: firstNode.ITD01,
+                  termsBasisDateCode: firstNode.ITD02
+                }
+              ],
+        dateTimeReference: getDateInformation(firstNode),
+        IT1_loop: [getItems(mainBody)],     
+        totalMonetaryValueSummary: [
+                {
+                  amount: firstNode.TDS01
+                }
+              ],
+        carrierDetails: [
+                {
+                  transportationMethodTypeCode: firstNode.CAD01,
+                  standardCarrierAlphaCode: firstNode.CAD04,
+                  referenceIdentificationQualifier: firstNode.CAD07,
+                  referenceIdentification: firstNode.CAD08
+                }
+              ],      
+        transactionTotals: [
+                {
+                        numberOfLineItems: firstNode.CTT01
+                }
+              ]
+      }
+    ]
+  }
+};
+options.data = response;
+///////// END CELIGO CODE ///////////////////////////////////
+
+/*
+Everything Above this is Celigo Ready.  Nothing Below this
+Does anything for you in Celigo, it is for testing only
+*/
+
+console.log('response', response);
+document.getElementById("response").innerHTML = JSON.stringify(response,null,2);
+
---
+++ src/Outbound855.mjs
@@ -0,0 +1,187 @@
+import './styles.css';
+import options from './855Nordstrom.json';
+document.getElementById("app").innerHTML = '855 Nordstom Script';
+// CELIGO PORTIONS BELOW THIS LINE
+/*
+Everything BETWEEN the CELIGO PORTIONS BELONGS IN CELIGO.  The
+rest if for development purposes and will do nothing in Celigo
+*/
+// We get the first record so we can use it to modify the header data BEFORE
+// going into the detail level issues.
+const mainBody = options.data[0];
+const firstNode = mainBody[0];
+const linecount = mainBody.length;
+/*  We're going to load the references as the array 
+    Building it as a function is untenebale.
+    We are also going to take into consideration the 
+    possibility that there may be only one.
+*/
+// REFERENCE INFORMATION FUNCTION
+const getReferenceInformation = node => {
+  const referenceInformation = [];
+// Make sure it is an array.  Then loop through it. If not
+// write the single records and move on.
+  if (Array.isArray(node.REF01)) {
+    node.REF01.forEach((ref01, index) => {
+      // For each value in node.REF01, we're going to create a new... OBJECT
+      const referenceInformationObject = {};
+      referenceInformationObject.referenceIdentificationQualifier = ref01;
+      if (node.REF02.length >= index) {
+        // If there's a corresponding value in node.REF02, we're going to add that data
+        // to the OBJECT
+        referenceInformationObject.referenceIdentification = node.REF02[index];
+      }
+      if (node.REF03 && node.REF03.length >= index) {
+        // If there's a corresponding value in node.REF03, we're going to add that data
+        // to the OBJECT
+        referenceInformationObject.description = node.REF03[index];
+      }
+      // Then we're going to push the OBJECT onto the referenceInformation ARRAY
+      referenceInformation.push(referenceInformationObject);
+    })
+  } else {
+    const referenceInformationObject = {};
+    referenceInformationObject.referenceIdentificationQualifier = node.REF01;
+    referenceInformationObject.referenceIdentification = node.REF02;
+    referenceInformationObject.description = node.REF03;
+    referenceInformation.push(referenceInformationObject);   
+  }
+  return referenceInformation;
+}
+/*
+
+There could be an array of Dates.  There could also be only ONE.  This
+function assures that the dates work both ways.
+
+*/
+//DATETIME INFORMATION FUNCTION
+const getDateInformation = node => {
+  const dateInformation = [];
+// Make sure it is an array.  Then loop through it. If not
+// write the single records and move on.
+  if (Array.isArray(node.DTM01)) {
+    node.DTM01.forEach((DTM01, index) => {
+      // For each value in node.DTM01, we're going to create a new... OBJECT
+      const dateInformationObject = {};
+      dateInformationObject.dateTimeQualifier = DTM01;
+      if (node.DTM02.length >= index) {
+        // If there's a corresponding value in node.DTM02, we're going to add that data
+        // to the OBJECT
+        dateInformationObject.date = node.DTM02[index];
+      }
+      if (node.DTM03 && node.DTM03.length >= index) {
+        // If there's a corresponding value in node.DTM03, we're going to add that data
+        // to the OBJECT
+        dateInformationObject.description = node.DTM03[index];
+      }
+      // Then we're going to push the OBJECT onto the dateInformation ARRAY
+      dateInformation.push(dateInformationObject);
+    })
+  } else {
+    const dateInformationObject = {};
+    dateInformationObject.dateTimeQualifier = node.DTM01;
+    dateInformationObject.dateTime = node.DTM02;
+    dateInformationObject.description = node.DTM03;
+    dateInformation.push(dateInformationObject);   
+  }
+  return dateInformation;
+}
+/*
+
+ Get the Items Themselves.  This is the only part that needs to 
+ loop through the entire record set.  Everything else is derived
+ from the very first record.  This is where the proverbial rubber meets
+ the road.
+
+*/
+const getItems = node => {
+  const itemInformation = [];
+  const destinationQ = [];
+  node.forEach((record, index) => {
+    // create the objects to load the data into
+    const itemInformationObject = {};
+    const proddestcobj ={};
+    // the loop for the baseline Item Data 
+    itemInformationObject.assignedIdentification = String(index+1);
+    itemInformationObject.quantity = record.PO102;
+    itemInformationObject.unitOrBasisForMeasurementCode = record.PO103;
+    itemInformationObject.unitPrice = record.PO104; 
+    itemInformationObject.basisOfUnitPriceCode = record.PO105;
+    itemInformationObject.productServiceIDQualifier = record.PO106;
+    itemInformationObject.productServiceID = record.PO107;
+    itemInformationObject.productServiceIDQualifier1 = record.PO108;
+    itemInformationObject.productServiceID1 = record.PO109;
+    itemInformation.push(itemInformationObject);
+    // the loop for the destination Quantity
+    proddestcobj.unitOrBasisForMeasurementCode = record.SDQ01;
+    proddestcobj.identificationCodeQualifier = record.SDQ02;
+    proddestcobj.identificationCode = record.SDQ03;
+    proddestcobj.quantity = record.SDQ04;
+    destinationQ.push(proddestcobj)
+  })
+  return {
+    baselineItemData: itemInformation, 
+    destinationQuantity: destinationQ
+  };
+}
+
+
+// MAIN RESPONSE OBJECT ///////////////////////////////
+/*
+This is where it is all assembled to go out to Orderful and
+all of the items are combined with the headers.
+*/
+const response ={
+  sender: {
+    isaId: firstNode.ISA06
+  },
+  receiver: {
+    isaId: firstNode.ISA08
+  },
+  type: {
+    name: firstNode.typeName
+  },
+  stream: firstNode.stream,
+  message: {
+    transactionSets: [
+      {
+        transactionSetHeader: [
+          {
+            transactionSetIdentifierCode: '855',
+            transactionSetControlNumber: '0001'
+          }
+        ],
+          beginningSegmentForPurchaseOrderAcknowledgment: [
+            {
+              transactionSetPurposeCode: firstNode.BAK01,
+              acknowledgmentTypeCode: firstNode.BAK02,
+              purchaseOrderNumber: firstNode.BAK03,
+              date: firstNode.BAK04
+            }
+          ],
+          referenceInformation: getReferenceInformation(firstNode),
+          dateTimeReference: getDateInformation(firstNode),
+          PO1_loop: [getItems(mainBody)],
+          CTT_loop: [
+            {
+              transactionTotals: [
+                {
+                  numberOfLineItems: String(linecount)
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  }
+  options.data = [response];
+///////// END CELIGO CODE ///////////////////////////////////
+
+/*
+Everything Above this is Celigo Ready.  Nothing Below this
+Does anything for you in Celigo, it is for testing only
+*/
+  console.log('response', options);
+  console.log('mainbody', mainBody);
+  document.getElementById("response").innerHTML = JSON.stringify(response,null,2);
