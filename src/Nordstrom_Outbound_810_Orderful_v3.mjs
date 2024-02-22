fc80285afbc6cee5f84921b1ecc654333eb80866
The Todd <68481229+ToddHill@users.noreply.github.com>
2024-02-21 21:46:56 -0500

    Update Nordstrom_Outbound_810_Orderful_v3.mjs
    Was able to get the grouping working and the updaterec working.  Both work on the preMap.  Now must use Celigo to process the updaterec array.

--- src/Nordstrom_Outbound_810_Orderful_v3.mjs
+++ src/Nordstrom_Outbound_810_Orderful_v3.mjs
@@ -1,10 +1,12 @@
+////////////////////////////// BEGIN DEVELOPMENT CODE //////////////////////////////
+// import options to the development environment
+// and execute the function that will build the data for the Orderful platform.
 import options from './Nordstrom_810_data.json' assert { type: "json" };
 preSavePage(options);
-
+////////////////////////////// END DEVELOPMENT CODE ////////////////////////////////
 
 ////////////////////////////// BEGIN CELIGO CODE ///////////////////////////////////
-
-// This function gets the items.
+// ITEMS AND TOTAL VALUE FUNCTION
 function getItems(node, ValueTotal) {
   let OrderTotal = 0;
   const items = node.map(record => {
@@ -40,212 +42,226 @@ function getItems(node, ValueTotal) {
     ValueTotal
   };
 }
-// This function is the main function that will be called to build the data for the Orderful platform.
-function preSavePage(options) {
-  const numberOfLineItems = options.data.length;
-  const data = []; 
-  let ValueTotal = 0;
-  let OrderTotal = 0;
-  let mainBody = options.data[0]; 
-  const firstNode = mainBody;
+// REFERENCE INFORMATION FUNCTION
+function getReferenceInformation(node) {
+      const referenceInformation = [];
+    // Make sure it is an array.  Then loop through it. If not
+    // write the single records and move on.
+      if (Array.isArray(node.REF01)) {
+        node.REF01.forEach((ref01, index) => {
+          // For each value in node.REF01, we're going to create a new... OBJECT
 
-  //  We're going to load the addresses as the array is so many levels deep that
-  //  Building it as a function is untenebale.
-  //
-  const addr = [];
-  addr.push(firstNode.N101);
-  const N1_loop = [];
-  const addressInformation = [];
-
-  for (let i=0; i < firstNode.N101.length; i++) {
-    const addressInformationObject = {};
-    const partyIdentificationObject ={};  
-      addressInformationObject.entityIdentifierCode = firstNode.N101[i];
-      if (firstNode.N102 && firstNode.N102[i]) {
-        addressInformationObject.name = firstNode.N102[i];
-    }
-      addressInformationObject.identificationCodeQualifier = firstNode.N103[i];
-      addressInformationObject.identificationCode = firstNode.N104[1];
-      addressInformation.push(addressInformationObject);
-      partyIdentificationObject.partyIdentification = [addressInformationObject];
-      N1_loop.push(partyIdentificationObject);
-    }
-  // Now, we place the functions to the top so we can begug them properly
-  // inserting the returns into the JSON itself to maintain integrity of 
-  // the loops and braces.
-  //
-  // REFERENCE INFORMATION FUNCTION
-  const getReferenceInformation = node => {
-    const referenceInformation = [];
-  // Make sure it is an array.  Then loop through it. If not
-  // write the single records and move on.
-    if (Array.isArray(node.REF01)) {
-      node.REF01.forEach((ref01, index) => {
-        // For each value in node.REF01, we're going to create a new... OBJECT
+          const referenceInformationObject = {};
+          referenceInformationObject.referenceIdentificationQualifier = ref01;
+          if (node.REF02.length >= index) {
+            // If there's a corresponding value in node.REF02, we're going to add that data
+            // to the OBJECT
+            referenceInformationObject.referenceIdentification = node.REF02[index];
+          }
+          if (node.REF03 && node.REF03.length >= index) {
+            // If there's a corresponding value in node.REF03, we're going to add that data
+            // to the OBJECT
+            referenceInformationObject.description = node.REF03[index];
+          }
+          // Then we're going to push the OBJECT onto the referenceInformation ARRAY
+          referenceInformation.push(referenceInformationObject);
+        })
+      } else {
 
         const referenceInformationObject = {};
-        referenceInformationObject.referenceIdentificationQualifier = ref01;
-        if (node.REF02.length >= index) {
-          // If there's a corresponding value in node.REF02, we're going to add that data
-          // to the OBJECT
-          referenceInformationObject.referenceIdentification = node.REF02[index];
-        }
-        if (node.REF03 && node.REF03.length >= index) {
-          // If there's a corresponding value in node.REF03, we're going to add that data
-          // to the OBJECT
-          referenceInformationObject.description = node.REF03[index];
-        }
-        // Then we're going to push the OBJECT onto the referenceInformation ARRAY
-        referenceInformation.push(referenceInformationObject);
-      })
-    } else {
+        referenceInformationObject.referenceIdentificationQualifier = node.REF01;
+        referenceInformationObject.referenceIdentification = node.REF02;
+        referenceInformationObject.description = node.REF03;
+        referenceInformation.push(referenceInformationObject);   
+      }
+      return referenceInformation;
+}
+//DATETIME INFORMATION FUNCTION
+function getDateInformation(node) {
+      const dateInformation = [];
+    // Make sure it is an array.  Then loop through it. If not
+    // write the single records and move on.
+      if (Array.isArray(node.DTM01)) {
+        node.DTM01.forEach((DTM01, index) => {
+          // For each value in node.DTM01, we're going to create a new... OBJECT
+          const dateInformationObject = {};
 
-      const referenceInformationObject = {};
-      referenceInformationObject.referenceIdentificationQualifier = node.REF01;
-      referenceInformationObject.referenceIdentification = node.REF02;
-      referenceInformationObject.description = node.REF03;
-      referenceInformation.push(referenceInformationObject);   
-    }
-    return referenceInformation;
-  }
-  // We will add a function to loop through the addresses the same way we 
-  // looped through the reference Identification information.
-  //DATETIME INFORMATION FUNCTION
-  const getDateInformation = node => {
-    const dateInformation = [];
-  // Make sure it is an array.  Then loop through it. If not
-  // write the single records and move on.
-    if (Array.isArray(node.DTM01)) {
-      node.DTM01.forEach((DTM01, index) => {
-        // For each value in node.DTM01, we're going to create a new... OBJECT
+          dateInformationObject.dateTimeQualifier = DTM01;
+          if (node.DTM02.length >= index) {
+            // If there's a corresponding value in node.DTM02, we're going to add that data
+            // to the OBJECT
+            dateInformationObject.date = node.DTM02[index];
+          }
+          if (node.DTM03 && node.DTM03.length >= index) {
+            // If there's a corresponding value in node.DTM03, we're going to add that data
+            // to the OBJECT
+            dateInformationObject.description = node.DTM03[index];
+          }
+          // Then we're going to push the OBJECT onto the dateInformation ARRAY
+          dateInformation.push(dateInformationObject);
+        })
+      } else {
         const dateInformationObject = {};
+        dateInformationObject.dateTimeQualifier = node.DTM01;
+        dateInformationObject.date = node.DTM02;
+        dateInformationObject.description = node.DTM03;
+        dateInformation.push(dateInformationObject);   
+      }
+      return dateInformation;
+}
+// ADDRESS INFORMATION FUNCTION
+function getAddressInformation(node) {
+  const addressInformation = [];
+  const partyIdentification = [];
+  const N1_loop = [];    
 
-        dateInformationObject.dateTimeQualifier = DTM01;
-        if (node.DTM02.length >= index) {
-          // If there's a corresponding value in node.DTM02, we're going to add that data
-          // to the OBJECT
-          dateInformationObject.date = node.DTM02[index];
-        }
-        if (node.DTM03 && node.DTM03.length >= index) {
-          // If there's a corresponding value in node.DTM03, we're going to add that data
-          // to the OBJECT
-          dateInformationObject.description = node.DTM03[index];
-        }
-        // Then we're going to push the OBJECT onto the dateInformation ARRAY
-        dateInformation.push(dateInformationObject);
-      })
-    } else {
-      const dateInformationObject = {};
-      dateInformationObject.dateTimeQualifier = node.DTM01;
-      dateInformationObject.date = node.DTM02;
-      dateInformationObject.description = node.DTM03;
-      dateInformation.push(dateInformationObject);   
-    }
-    return dateInformation;
-  }
-  //
-  // ADDRESS INFORMATION FUNCTION
-  const getAddressInformation = node => {
-    const addressInformation = [];
-    const partyIdentification =[];
-    node.N101.forEach((n101, index) => {
-      const addressInformationObject = {};
-      const partyIdentificationObject ={};
-      addressInformationObject.entityIdentifierCode = n101;
+  node.N101.forEach((n101, index) => {
+    const addressInformationObject = {};
+    const partyIdentificationObject = {};
+    addressInformationObject.entityIdentifierCode = n101;
+    
+    // Check if node.N102 is defined and has elements before accessing its elements
+    if (node.N102 && node.N102[index]) {
       addressInformationObject.name = node.N102[index];
-      addressInformationObject.identificationCodeQualifier = node.N103[index];
-      addressInformationObject.identificationCode = node.N104[index];
-      addressInformation.push(addressInformationObject);
-      partyIdentificationObject.partyIdentification = [addressInformationObject];
-      N1_loop.push(partyIdentificationObject);
-    })
-    return {
-      N1_loop
-    };
-  }
-
-//
-const items = getItems(options.data, ValueTotal).items;
-ValueTotal = getItems(options.data, ValueTotal).ValueTotal;
-
-const totalMonetaryValueSummary = {
-  amount: ValueTotal.toFixed(2).replace('.', '')
-};
-
-  // Main Response, the content is built here.  This is what will be sent
-  // to Orderful for processing.  All of the previous lines are functions
-  // that inform this constant.
-  //
-  const response = [{
-    sender: {
-      isaId: firstNode.ISA06
-    },
-    receiver: {
-      isaId: firstNode.ISA08
-    },
-    type: {
-      name: firstNode.typeName
-    },
-    stream: firstNode.stream,
-    message: {
-      transactionSets: [
-        {
-          transactionSetHeader: [
-            {
-              transactionSetIdentifierCode: '810',
-              transactionSetControlNumber: '0001'
-            }
-          ],
-          beginningSegmentForInvoice: [
-            {
-              date: firstNode.BIG01,
-              invoiceNumber: firstNode.id,
-              date1: firstNode.BIG03,
-              purchaseOrderNumber: firstNode.BIG04,
-              releaseNumber: firstNode.BIG05,
-              changeOrderSequenceNumber: firstNode.BIG06,
-              transactionTypeCode: firstNode.BIG07
-            }
-          ],
-          referenceInformation: getReferenceInformation(firstNode), 
-          N1_loop,
-          termsOfSaleDeferredTermsOfSale: [
-                  {
-                    termsTypeCode: firstNode.ITD01,
-                    termsBasisDateCode: firstNode.ITD02
-                  }
-                ],
-          dateTimeReference: getDateInformation(firstNode),
-          IT1_loop: items,    
-          totalMonetaryValueSummary: [totalMonetaryValueSummary],
-          carrierDetails: [
-                  {
-                    transportationMethodTypeCode: firstNode.CAD01,
-                    standardCarrierAlphaCode: firstNode.CAD04,
-                    referenceIdentificationQualifier: firstNode.CAD07,
-                    referenceIdentification: firstNode.CAD08
-                  }
-                ],      
-          transactionTotals: [
-                  {
-                          numberOfLineItems: numberOfLineItems.toString()
-                  }
-                ]
-        }
-      ]
     }
-  }];
-  response[0].updaterec = firstNode.id;
-  data.push(response);
-  console.log(JSON.stringify(data, null, 2));
+    
+    // Check if node.N103 is defined and has elements before accessing its elements
+    if (node.N103 && node.N103[index]) {
+      addressInformationObject.identificationCodeQualifier = node.N103[index];
+    }
+    
+    // Check if node.N104 is defined and has elements before accessing its elements
+    if (node.N104 && node.N104[index]) {
+      addressInformationObject.identificationCode = node.N104[1];
+    }
+    addressInformation.push(addressInformationObject);
+    partyIdentificationObject.partyIdentification = [addressInformationObject];
+    N1_loop.push(partyIdentificationObject);
+  });
+
+  
+  // Return the address information object
+  return {
+    N1_loop
+  };
+}
+// PRESAVE PAGE - where the work gets done.
+// it returns the main response that will be sent to Orderful for processing.
+function preSavePage(options) {
+  const data = []; 
+  // Group items by N104[1]
+  const groupedItems = {};
+  options.data.forEach(item => {
+    const key = item.N104[1];
+    if (!groupedItems[key]) {
+      groupedItems[key] = [];
+    }
+    groupedItems[key].push(item);
+  });
+  
+  // Iterate over grouped items
+  for (const key in groupedItems) {
+    const groupedItemsArray = groupedItems[key];
+    let ValueTotal = 0; // Initialize ValueTotal here
+    const numberOfLineItems = groupedItemsArray.length;
+    
+    // Calculate total value
+    groupedItemsArray.forEach(record => {
+      const OrderTotal = record.IT102 * record.IT104;
+      ValueTotal += OrderTotal;
+    });
+    // Calculate totalMonetaryValueSummary for this group
+    const totalMonetaryValueSummary = {
+      amount: ValueTotal.toFixed(2).replace('.', '')
+    };
+    // Iterate over grouped items array to create response objects 
+    const responses = [];
+    groupedItemsArray.forEach(item => {
+      let mainBody = item; // Use the current item here
+      // Get items
+      const { items, ValueTotal: updatedValueTotal } = getItems(groupedItemsArray, ValueTotal);
+
+      // construct the response object  
+      const response = {
+        sender: {
+          isaId: mainBody.ISA06
+        },
+        receiver: {
+          isaId: mainBody.ISA08
+        },
+        type: {
+          name: mainBody.typeName
+        },
+        stream: mainBody.stream,
+        message: {
+          transactionSets: [
+            {
+              transactionSetHeader: [
+                {
+                  transactionSetIdentifierCode: '810',
+                  transactionSetControlNumber: '0001'
+                }
+              ],
+              beginningSegmentForInvoice: [
+                {
+                  date: mainBody.BIG01,
+                  invoiceNumber: mainBody.id,
+                  date1: mainBody.BIG03,
+                  purchaseOrderNumber: mainBody.BIG04,
+                  releaseNumber: mainBody.BIG05,
+                  changeOrderSequenceNumber: mainBody.BIG06,
+                  transactionTypeCode: mainBody.BIG07
+                }
+              ],
+              referenceInformation: getReferenceInformation(mainBody),
+              N1_loop: getAddressInformation(mainBody), // Call getAddressInformation here
+              termsOfSaleDeferredTermsOfSale: [
+                {
+                  termsTypeCode: mainBody.ITD01,
+                  termsBasisDateCode: mainBody.ITD02
+                }
+              ],
+              dateTimeReference: getDateInformation(mainBody),
+              IT1_loop: items,
+              totalMonetaryValueSummary: [totalMonetaryValueSummary],
+              carrierDetails: [
+                {
+                  transportationMethodTypeCode: mainBody.CAD01,
+                  standardCarrierAlphaCode: mainBody.CAD04,
+                  referenceIdentificationQualifier: mainBody.CAD07,
+                  referenceIdentification: mainBody.CAD08
+                }
+              ],
+              transactionTotals: [
+                {
+                  numberOfLineItems: numberOfLineItems.toString()
+                }
+              ]
+            }
+          ]
+        }
+      };
+  // Initialize an array to store IDs
+
+
+  // Initialize an array to store IDs
+  const allIds = groupedItemsArray.map(item => item.id);
+
+  // Assign the array of IDs to the updaterec field
+  response.updaterec = allIds;
+
+  // PUSH THE RESPONSE OBJECT INTO THE RESPONSES ARRAY 
+      responses.push(response);
+    });
+    data.push(responses);
+    console.log(JSON.stringify(data, null, 2));
+  } 
   
   return {
     data: data,
     errors: options.errors,
     settings: options.settings,
     testMode: options.testMode
-    
   };
-
-  };
-///////// END CELIGO CODE //////////////////////////////////////////////////////////
+}
+////////////////////////////// END CELIGO CODE ////////////////////////////////////~
