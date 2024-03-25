f36ef479baa2221f23db981be75296ce1120b114
The Todd <68481229+ToddHill@users.noreply.github.com>
2024-03-25 16:20:29 -0400

    Update to Bloomingdales 856

--- src/Bloomingdales_Outbound_856_Orderful_v3.mjs
+++ src/Bloomingdales_Outbound_856_Orderful_v3.mjs
@@ -1,486 +1,6 @@
-//
-// Development Setup for 856
-// includes options.data as inclusive to simulate
-// Celigo Setup
-// Begin By Loading options.data
-
-const options = {
-  data: [
-    {
-      id: "33343249",
-      recordType: "itemfulfillment",
-      updaterec: "33342646",
-      ISA06: "BHGBEIS",
-      ISA08: "6113310072",
-      typeName: "856_SHIP_NOTICE_MANIFEST",
-      stream: "Test",
-      BSN01: "00",
-      BSN02: "0123456789-test",
-      BSN03: "20231218",
-      BSN04: "111211",
-      TD101: "CTN25",
-      TD102: "0",
-      TD106: "G",
-      TD107: "5",
-      TD108: "LB",
-      TD504: "M",
-      TD505: "TODD",
-      TD506: "CC",
-      REF01: "BM",
-      REF02: "0123456789-test",
-      DTM01: ["011", "067"],
-      DTM02: ["20231218", "20231220"],
-      FOB01: "CC",
-      N101: ["ST", "BY", "SF"],
-      N102: ["Secaucus DC", "Bloomingdales", "Beis"],
-      N103: ["92", "92", "92"],
-      N104: ["SC", "0001", "99999"],
-      PRF01: "5105203",
-      PRF04: "20231215",
-      N401: "El Segundo",
-      N402: "CA",
-      MAN01: "GM",
-      MAN02: "00008100128601750001",
-      PID01: "S",
-      PID03: "VI",
-      PID04: "FL",
-      LIN02: "UP",
-      LIN03: "810012865272",
-      LIN04: "VA",
-      LIN05: "BEIS223428",
-      SN102: "1",
-      SN103: "EA",
-      dataURI:
-        "https://4675206-sb1.app.netsuite.com/app/accounting/transactions/itemship.nl?id=33343249&compid=4675206_SB1",
-    },
-    {
-      id: "33343250",
-      recordType: "itemfulfillment",
-      updaterec: "33342649",
-      ISA06: "BHGBEIS",
-      ISA08: "6113310072",
-      typeName: "856_SHIP_NOTICE_MANIFEST",
-      stream: "Test",
-      BSN01: "00",
-      BSN02: "0123456789-test",
-      BSN03: "20231218",
-      BSN04: "111241",
-      TD101: "CTN25",
-      TD102: "0",
-      TD106: "G",
-      TD107: ".01",
-      TD108: "LB",
-      TD504: "M",
-      TD505: "TODD",
-      TD506: "CC",
-      REF01: "BM",
-      REF02: "0123456789-test",
-      DTM01: ["011", "067"],
-      DTM02: ["20231218", "20231226"],
-      FOB01: "CC",
-      N101: ["ST", "BY", "SF"],
-      N102: ["Secaucus DC", "Bloomingdales", "Beis"],
-      N103: ["92", "92", "92"],
-      N104: ["SC", "0004", "99999"],
-      PRF01: "5105203",
-      PRF04: "20231215",
-      N401: "El Segundo",
-      N402: "CA",
-      MAN01: "GM",
-      MAN02: "00008100128601750002",
-      PID01: "S",
-      PID03: "VI",
-      PID04: "FL",
-      LIN02: "UP",
-      LIN03: "810012865272",
-      LIN04: "VA",
-      LIN05: "BEIS223428",
-      SN102: "1",
-      SN103: "EA",
-      dataURI:
-        "https://4675206-sb1.app.netsuite.com/app/accounting/transactions/itemship.nl?id=33343250&compid=4675206_SB1",
-    },
-    {
-      id: "33343251",
-      recordType: "itemfulfillment",
-      updaterec: "33342651",
-      ISA06: "BHGBEIS",
-      ISA08: "6113310072",
-      typeName: "856_SHIP_NOTICE_MANIFEST",
-      stream: "Test",
-      BSN01: "00",
-      BSN02: "0123456789-test",
-      BSN03: "20231218",
-      BSN04: "111204",
-      TD101: "CTN25",
-      TD102: "0",
-      TD106: "G",
-      TD107: ".01",
-      TD108: "LB",
-      TD504: "M",
-      TD505: "TODD",
-      TD506: "CC",
-      REF01: "BM",
-      REF02: "0123456789-test",
-      DTM01: ["011", "067"],
-      DTM02: ["20231218", "20231228"],
-      FOB01: "CC",
-      N101: ["ST", "BY", "SF"],
-      N102: ["Secaucus DC", "Bloomingdales", "Beis"],
-      N103: ["92", "92", "92"],
-      N104: ["SC", "0006", "99999"],
-      PRF01: "5105203",
-      PRF04: "20231215",
-      N401: "El Segundo",
-      N402: "CA",
-      MAN01: "GM",
-      MAN02: "00008100128601750004",
-      PID01: "S",
-      PID03: "VI",
-      PID04: "FL",
-      LIN02: "UP",
-      LIN03: "810012865272",
-      LIN04: "VA",
-      LIN05: "BEIS223428",
-      SN102: "1",
-      SN103: "EA",
-      dataURI:
-        "https://4675206-sb1.app.netsuite.com/app/accounting/transactions/itemship.nl?id=33343251&compid=4675206_SB1",
-    },
-    {
-      id: "33343252",
-      recordType: "itemfulfillment",
-      updaterec: "33342650",
-      ISA06: "BHGBEIS",
-      ISA08: "6113310072",
-      typeName: "856_SHIP_NOTICE_MANIFEST",
-      stream: "Test",
-      BSN01: "00",
-      BSN02: "0123456789-test",
-      BSN03: "20231218",
-      BSN04: "111212",
-      TD101: "CTN25",
-      TD102: "0",
-      TD106: "G",
-      TD107: ".01",
-      TD108: "LB",
-      TD504: "M",
-      TD505: "TODD",
-      TD506: "CC",
-      REF01: "BM",
-      REF02: "0123456789-test",
-      DTM01: ["011", "067"],
-      DTM02: ["20231218", "20231226"],
-      FOB01: "CC",
-      N101: ["ST", "BY", "SF"],
-      N102: ["Secaucus DC", "Bloomingdales", "Beis"],
-      N103: ["92", "92", "92"],
-      N104: ["SC", "0005", "99999"],
-      PRF01: "5105203",
-      PRF04: "20231215",
-      N401: "El Segundo",
-      N402: "CA",
-      MAN01: "GM",
-      MAN02: "00008100128601750003",
-      PID01: "S",
-      PID03: "VI",
-      PID04: "FL",
-      LIN02: "UP",
-      LIN03: "810012865272",
-      LIN04: "VA",
-      LIN05: "BEIS223428",
-      SN102: "1",
-      SN103: "EA",
-      dataURI:
-        "https://4675206-sb1.app.netsuite.com/app/accounting/transactions/itemship.nl?id=33343252&compid=4675206_SB1",
-    },
-    {
-      id: "33343253",
-      recordType: "itemfulfillment",
-      updaterec: "33342654",
-      ISA06: "BHGBEIS",
-      ISA08: "6113310072",
-      typeName: "856_SHIP_NOTICE_MANIFEST",
-      stream: "Test",
-      BSN01: "00",
-      BSN02: "0123456789-test",
-      BSN03: "20231218",
-      BSN04: "111222",
-      TD101: "CTN25",
-      TD102: "0",
-      TD106: "G",
-      TD107: ".01",
-      TD108: "LB",
-      TD504: "M",
-      TD505: "TODD",
-      TD506: "CC",
-      REF01: "BM",
-      REF02: "0123456789-test",
-      DTM01: ["011", "067"],
-      DTM02: ["20231218", "20231226"],
-      FOB01: "CC",
-      N101: ["ST", "BY", "SF"],
-      N102: ["Secaucus DC", "Bloomingdales", "Beis"],
-      N103: ["92", "92", "92"],
-      N104: ["SC", "0024", "99999"],
-      PRF01: "5105203",
-      PRF04: "20231215",
-      N401: "El Segundo",
-      N402: "CA",
-      MAN01: "GM",
-      MAN02: "00008100128601750005",
-      PID01: "S",
-      PID03: "VI",
-      PID04: "FL",
-      LIN02: "UP",
-      LIN03: "810012865272",
-      LIN04: "VA",
-      LIN05: "BEIS223428",
-      SN102: "1",
-      SN103: "EA",
-      dataURI:
-        "https://4675206-sb1.app.netsuite.com/app/accounting/transactions/itemship.nl?id=33343253&compid=4675206_SB1",
-    },
-    {
-      id: "33344463",
-      recordType: "itemfulfillment",
-      updaterec: "33342655",
-      ISA06: "BHGBEIS",
-      ISA08: "6113310072",
-      typeName: "856_SHIP_NOTICE_MANIFEST",
-      stream: "Test",
-      BSN01: "00",
-      BSN02: "0123456789-test2",
-      BSN03: "20240103",
-      BSN04: "080104",
-      TD101: "CTN25",
-      TD102: "0",
-      TD106: "G",
-      TD107: ".01",
-      TD108: "LB",
-      TD504: "M",
-      TD505: "TODD",
-      TD506: "CC",
-      REF01: "BM",
-      REF02: "0123456789-test2",
-      DTM01: ["011", "067"],
-      DTM02: ["20240103", "20240108"],
-      FOB01: "CC",
-      N101: ["ST", "BY", "SF"],
-      N102: ["Stone Mountain DC", "Bloomingdales", "Beis"],
-      N103: ["92", "92", "92"],
-      N104: ["ST", "0055", "99999"],
-      PRF01: "5105203",
-      PRF04: "20231215",
-      N401: "El Segundo",
-      N402: "CA",
-      MAN01: "GM",
-      MAN02: "00008100128601750006",
-      PID01: "S",
-      PID03: "VI",
-      PID04: "FL",
-      LIN02: "UP",
-      LIN03: "810012865272",
-      LIN04: "VA",
-      LIN05: "BEIS223428",
-      SN102: "1",
-      SN103: "EA",
-      dataURI:
-        "https://4675206-sb1.app.netsuite.com/app/accounting/transactions/itemship.nl?id=33344463&compid=4675206_SB1",
-    },
-    {
-      id: "33344465",
-      recordType: "itemfulfillment",
-      updaterec: "33342653",
-      ISA06: "BHGBEIS",
-      ISA08: "6113310072",
-      typeName: "856_SHIP_NOTICE_MANIFEST",
-      stream: "Test",
-      BSN01: "00",
-      BSN02: "0123456789-test2",
-      BSN03: "20240103",
-      BSN04: "080121",
-      TD101: "CTN25",
-      TD102: "0",
-      TD106: "G",
-      TD107: ".01",
-      TD108: "LB",
-      TD504: "M",
-      TD505: "TODD",
-      TD506: "CC",
-      REF01: "BM",
-      REF02: "0123456789-test2",
-      DTM01: ["011", "067"],
-      DTM02: ["20240103", "20240108"],
-      FOB01: "CC",
-      N101: ["ST", "BY", "SF"],
-      N102: ["Stone Mountain DC", "Bloomingdales", "Beis"],
-      N103: ["92", "92", "92"],
-      N104: ["ST", "0020", "99999"],
-      PRF01: "5105203",
-      PRF04: "20231215",
-      N401: "El Segundo",
-      N402: "CA",
-      MAN01: "GM",
-      MAN02: "00008100128601750007",
-      PID01: "S",
-      PID03: "VI",
-      PID04: "FL",
-      LIN02: "UP",
-      LIN03: "810012865272",
-      LIN04: "VA",
-      LIN05: "BEIS223428",
-      SN102: "1",
-      SN103: "EA",
-      dataURI:
-        "https://4675206-sb1.app.netsuite.com/app/accounting/transactions/itemship.nl?id=33344465&compid=4675206_SB1",
-    },
-    {
-      id: "33344467",
-      recordType: "itemfulfillment",
-      updaterec: "33342652",
-      ISA06: "BHGBEIS",
-      ISA08: "6113310072",
-      typeName: "856_SHIP_NOTICE_MANIFEST",
-      stream: "Test",
-      BSN01: "00",
-      BSN02: "0123456789-test2",
-      BSN03: "20240103",
-      BSN04: "090135",
-      TD101: "CTN25",
-      TD102: "0",
-      TD106: "G",
-      TD107: ".01",
-      TD108: "LB",
-      TD504: "M",
-      TD505: "TODD",
-      TD506: "CC",
-      REF01: "BM",
-      REF02: "0123456789-test2",
-      DTM01: ["011", "067"],
-      DTM02: ["20240103", "20240108"],
-      FOB01: "CC",
-      N101: ["ST", "BY", "SF"],
-      N102: ["Stone Mountain DC", "Bloomingdales", "Beis"],
-      N103: ["92", "92", "92"],
-      N104: ["ST", "0010", "99999"],
-      PRF01: "5105203",
-      PRF04: "20231215",
-      N401: "El Segundo",
-      N402: "CA",
-      MAN01: "GM",
-      MAN02: "00008100128601750009",
-      PID01: "S",
-      PID03: "VI",
-      PID04: "FL",
-      LIN02: "UP",
-      LIN03: "810012865272",
-      LIN04: "VA",
-      LIN05: "BEIS223428",
-      SN102: "1",
-      SN103: "EA",
-      dataURI:
-        "https://4675206-sb1.app.netsuite.com/app/accounting/transactions/itemship.nl?id=33344467&compid=4675206_SB1",
-    },
-    {
-      id: "33344469",
-      recordType: "itemfulfillment",
-      updaterec: "33342648",
-      ISA06: "BHGBEIS",
-      ISA08: "6113310072",
-      typeName: "856_SHIP_NOTICE_MANIFEST",
-      stream: "Test",
-      BSN01: "00",
-      BSN02: "0123456789-test2",
-      BSN03: "20240103",
-      BSN04: "090157",
-      TD101: "CTN25",
-      TD102: "0",
-      TD106: "G",
-      TD107: ".01",
-      TD108: "LB",
-      TD504: "M",
-      TD505: "TODD",
-      TD506: "CC",
-      REF01: "BM",
-      REF02: "0123456789-test2",
-      DTM01: ["011", "067"],
-      DTM02: ["20240103", "20240108"],
-      FOB01: "CC",
-      N101: ["ST", "BY", "SF"],
-      N102: ["Stone Mountain DC", "Bloomingdales", "Beis"],
-      N103: ["92", "92", "92"],
-      N104: ["ST", "0003", "99999"],
-      PRF01: "5105203",
-      PRF04: "20231215",
-      N401: "El Segundo",
-      N402: "CA",
-      MAN01: "GM",
-      MAN02: "00008100128601750008",
-      PID01: "S",
-      PID03: "VI",
-      PID04: "FL",
-      LIN02: "UP",
-      LIN03: "810012865272",
-      LIN04: "VA",
-      LIN05: "BEIS223428",
-      SN102: "1",
-      SN103: "EA",
-      dataURI:
-        "https://4675206-sb1.app.netsuite.com/app/accounting/transactions/itemship.nl?id=33344469&compid=4675206_SB1",
-    },
-    {
-      id: "33344471",
-      recordType: "itemfulfillment",
-      updaterec: "33342647",
-      ISA06: "BHGBEIS",
-      ISA08: "6113310072",
-      typeName: "856_SHIP_NOTICE_MANIFEST",
-      stream: "Test",
-      BSN01: "00",
-      BSN02: "0123456789-test2",
-      BSN03: "20240103",
-      BSN04: "090133",
-      TD101: "CTN25",
-      TD102: "0",
-      TD106: "G",
-      TD107: ".01",
-      TD108: "LB",
-      TD504: "M",
-      TD505: "TODD",
-      TD506: "CC",
-      REF01: "BM",
-      REF02: "0123456789-test2",
-      DTM01: ["011", "067"],
-      DTM02: ["20240103", "20240108"],
-      FOB01: "CC",
-      N101: ["ST", "BY", "SF"],
-      N102: ["Stone Mountain DC", "Bloomingdales", "Beis"],
-      N103: ["92", "92", "92"],
-      N104: ["ST", "0002", "99999"],
-      PRF01: "5105203",
-      PRF04: "20231215",
-      N401: "El Segundo",
-      N402: "CA",
-      MAN01: "GM",
-      MAN02: "000081001286017500010",
-      PID01: "S",
-      PID03: "VI",
-      PID04: "FL",
-      LIN02: "UP",
-      LIN03: "810012865272",
-      LIN04: "VA",
-      LIN05: "BEIS223428",
-      SN102: "1",
-      SN103: "EA",
-      dataURI:
-        "https://4675206-sb1.app.netsuite.com/app/accounting/transactions/itemship.nl?id=33344471&compid=4675206_SB1",
-    },
-  ],
-};
+// Version: 1.3.1
+import options from './Bloomingdales_856_data.json' assert { type: "json" };
 preSavePage(options);
-
-
 /////////////////////////////////////////////////////////////////////////////////////////////
 //                                                                                         //
 // Everything BELOW this block goes to Celigo.  Everything ABOVE this block is development //
@@ -506,8 +26,9 @@ Release History:
 1.0.5 - 2024-01-30 - set shipping weight using toFixed(2) to eliminate extra decimals. Ended up with entirely new variable to do this.
 1.1.0 - 2024-02-08 - Added updaterec to the Response Object for updating the record in NetSuite.
 1.2.0 - 2024-03-08 - Added objects to the updaterec from the UpdateRecordsArray to be sent to NetSuite.
+1.3.0 - 2024-03-25 - Added the UpdateRecordsArray to the preSavePage function to collect the updateid for the updaterec object.
+1.3.1 - 2024-03-25 - Brought the decimals to two and added the total lines
 */
-
 
 function preSavePage(options) {
   let UpdateRecordsArray = [];
@@ -540,6 +61,11 @@ function preSavePage(options) {
       }
       let bolObj = newRecord[bolKey];
     }
+    console.log(totalLines);
+    // Add the total transaction lines to the newRecord object
+    let numberOfLines = totalLines.toString();
+    newRecord[bolKey]["message"]["transactionSets"][0]["transactionTotals"] = [];
+    newRecord[bolKey]["message"]["transactionSets"][0]["transactionTotals"].push({numberOfLineItems: numberOfLines});
     newRecord[bolKey]["dcObj"] = undefined;
 
 
@@ -624,6 +150,7 @@ function combineByBol(oldRecord, newRecord, newOptions) {
       };
 // update the shipmentladingQuantity and shipmentWeight
       var shortWeight = parseFloat(oldRecord[i].TD107);
+      shortWeight = parseFloat(shortWeight.toFixed(2));
       newOptions[bolKey]["shipmentladingQuantity"] = newOptions[bolKey]["shipmentladingQuantity"] + 1;
       newOptions[bolKey]["shipmentWeight"] = newOptions[bolKey]["shipmentWeight"] + shortWeight;
 // update the orderladingQuantity and orderWeight
@@ -704,15 +231,11 @@ function combineByBol(oldRecord, newRecord, newOptions) {
             if (node.REF02.length >= index) {
               // If there's a corresponding value in node.REF02, we're going to add that data
               // to the OBJECT
-              referenceInformationObject.referenceIdentification =
-                node.REF02[index];
+              referenceInformationObject.referenceIdentification = node.REF02[index];
             }
             if (node.REF03 && node.REF03.length >= index) {
-              // If there's a corresponding value in node.REF03, we're going to add that data
-              // to the OBJECT
               referenceInformationObject.description = node.REF03[index];
             }
-            // Then we're going to push the OBJECT onto the referenceInformation ARRAY
             referenceInformation.push(referenceInformationObject);
           });
         } else {
@@ -769,20 +292,39 @@ function combineByBol(oldRecord, newRecord, newOptions) {
       addr.push(oldRecord[i].N101);
       const N1_loop = [];
       const addressInformation = [];
-      for (let n = 0; n < oldRecord[i].N101.length; n++) {
-        const addressInformationObject = {};
-        const partyIdentificationObject = {};
-        addressInformationObject.entityIdentifierCode = oldRecord[i].N101[n];
-        addressInformationObject.name = oldRecord[i].N102[n];
-        addressInformationObject.identificationCodeQualifier =
-          oldRecord[i].N103[n];
-        addressInformationObject.identificationCode = oldRecord[i].N104[n];
+      let addressInformationObject = {};
+      let partyIdentificationObject = {};
+      let geographicLocationItem = {};
+      let geographicLocationObject = {};
+        addressInformationObject.entityIdentifierCode = oldRecord[i].N101[0];
+        addressInformationObject.name = oldRecord[i].N102[0];
+        addressInformationObject.identificationCodeQualifier = oldRecord[i].N103[0];
+        addressInformationObject.identificationCode = oldRecord[i].N104[0];
         addressInformation.push(addressInformationObject);
         partyIdentificationObject.partyIdentification = [
           addressInformationObject,
         ];
         N1_loop.push(partyIdentificationObject);
-      }
+        addressInformationObject = {};
+        partyIdentificationObject = {};
+        
+        geographicLocationItem = {};
+        addressInformationObject.entityIdentifierCode = oldRecord[i].N101[2];
+        addressInformationObject.name = oldRecord[i].N102[2];
+        addressInformationObject.identificationCodeQualifier = oldRecord[i].N103[2];
+        addressInformationObject.identificationCode = oldRecord[i].N104[2];
+        addressInformation.push(addressInformationObject);
+
+        partyIdentificationObject.partyIdentification = [
+          addressInformationObject,
+        ];
+        geographicLocationItem.cityName = oldRecord[i].N401;
+        geographicLocationItem.stateOrProvinceCode = oldRecord[i].N402;
+        partyIdentificationObject.geographicLocation = [
+          geographicLocationItem
+        ];
+        N1_loop.push(partyIdentificationObject); 
+           
       // End Address Information
 
       // Reference Information
