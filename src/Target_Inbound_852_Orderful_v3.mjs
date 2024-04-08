b5ffe7d26f917808a15e51a94c754387c0524e20
The Todd <68481229+ToddHill@users.noreply.github.com>
2024-04-08 17:26:55 -0400

    Having run the loop, we had to rebuild the group for the Target 852

--- src/Target_Inbound_852_Orderful_v3.mjs
+++ src/Target_Inbound_852_Orderful_v3.mjs
@@ -1,6 +1,6 @@
-import options  from "./data/Target_852_data_shortform.json" assert { type: "json"  };
+import options  from "./data/Target_852_data.json" assert { type: "json"  };
 preSavePage(options);
-
+console.log(JSON.stringify(options, null, 2));
 //////////////////////////////////////////////////////////////////////////////////////////
 /*
 Target 852 Processing
@@ -21,55 +21,59 @@ For use in the Analytic Features Built into NetSuite.
 4.08.2024 - Discovered that all indentificationCode and quantity fields are not being read. Added a loop to account for this.
 */
 ///////////////////////////////// BEGIN CELIGO CODE /////////////////////////////////////////////////////////////
-
 function preSavePage(options) {
   const data = options.data;
-  const flattenedData = [];
-
-  // Retrieve relevant data
-  const retailerID = "2276213"; // Set the RetailerID
+  const groupedData = {};
 
   // Process transaction sets
-  for (const transactionSet of data[0].message.content.transactionSets) {
+  for (const transactionSet of data[0].message.transactionSets) {
     const reportingDate = convertDate(transactionSet.reportingDateAction[0].date);
 
     for (const LIN_loop_item of transactionSet.LIN_loop) {
       const productServiceID2 = LIN_loop_item.itemIdentification[0].productServiceID2;
 
       for (const ZA_loop_item of LIN_loop_item.ZA_loop) {
-        let currentQuantity = 0;
-        let soldQuantity = 0;
-        let onOrderQuantity = 0;
+        const storeCode = ZA_loop_item.destinationQuantity[0].identificationCode;
 
         for (const activity of ZA_loop_item.productActivityReporting) {
-          const qty = ZA_loop_item.destinationQuantity[0].quantity;
+          const qty = parseInt(ZA_loop_item.destinationQuantity[0].quantity);
+
+          let currentQuantity = 0;
+          let soldQuantity = 0;
+          let onOrderQuantity = 0;
 
           if (activity.activityCode === "QA") {
-            currentQuantity = parseInt(qty);
+            currentQuantity = qty;
           } else if (activity.activityCode === "QS") {
-            soldQuantity = parseInt(qty);
+            soldQuantity = qty;
           } else if (activity.activityCode === "QP") {
-            onOrderQuantity = parseInt(qty);
+            onOrderQuantity = qty;
           }
-        }
 
-        for (const destinationQuantity of ZA_loop_item.destinationQuantity) {
-          const identificationCode = destinationQuantity.identificationCode;
+          const key = `${reportingDate}_${productServiceID2}_${storeCode}`;
+          if (!groupedData[key]) {
+            groupedData[key] = {
+              RetailerId: "2276213",
+              date: reportingDate,
+              item: productServiceID2,
+              storeCode: storeCode,
+              currentQuantity: 0,
+              soldQuantity: 0,
+              onOrderQuantity: 0
+            };
+          }
 
-          // Push processed data to flattenedData array
-          flattenedData.push({
-            RetailerId: retailerID,
-            date: reportingDate,
-            item: productServiceID2,
-            storeCode: identificationCode,
-            currentQuantity: currentQuantity,
-            soldQuantity: soldQuantity,
-            onOrderQuantity: onOrderQuantity
-          });
+          // Update quantities for the same key
+          groupedData[key].currentQuantity += currentQuantity;
+          groupedData[key].soldQuantity += soldQuantity;
+          groupedData[key].onOrderQuantity += onOrderQuantity;
         }
       }
     }
   }
+
+  // Convert groupedData object to array
+  const flattenedData = Object.values(groupedData);
 
   // Assign flattenedData to MainObject
   data[0].MainObject = flattenedData;
@@ -90,6 +94,5 @@ function convertDate(date) {
   var day = date.slice(6, 8);
   return `${month}/${day}/${year}`;
 }
-
 
 ///////////////////////////////// END CELIGO CODE /////////////////////////////////////////////////////////////
